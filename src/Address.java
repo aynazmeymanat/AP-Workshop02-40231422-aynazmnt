f07a32dca08ac119207bab94cc81e034c74bf217
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-25 13:31:15 +0330

    methoda , getter and setter were writed

--- src/Address.java
+++ src/Address.java
@@ -14,4 +14,38 @@ public class Address {
         this.CodePosty = CodePosty;
     }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCodePosty() {
+        return CodePosty;
+    }
+
+    public void setCodePosty(String CodePosty) {
+        if (CodePosty.length() == 5) {
+            this.CodePosty = CodePosty;
+        } else {
+            System.out.println("invalid");
+            System.exit(0);
+        }
+    }
+
+    public void printAddress() {
+        System.out.println("City: " + city);
+        System.out.println("Street: " + street);
+        System.out.println("CodePosty: " + CodePosty);
+    }
 }
