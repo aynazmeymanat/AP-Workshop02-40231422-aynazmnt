a54cb3c5fa97028a2b265e8f6087d9d7303853f4
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-25 13:28:39 +0330

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,17 @@
 public class Address {
+        private String city;
+        private String street;
+        private String CodePosty;
+
+    public Address(String city, String street, String CodePosty) {
+        if (CodePosty.length() == 5) {
+            this.city = city;
+        } else {
+            System.out.println("invalid");
+            System.exit(0);
+        }
+        this.street = street;
+        this.CodePosty = CodePosty;
+    }
+
 }
