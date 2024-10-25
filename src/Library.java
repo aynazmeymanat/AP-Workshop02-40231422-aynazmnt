564de9ddb74242f4ebd5d07b599c18ae1298abf6
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-25 14:04:20 +0330

    fields and constructor were written

--- src/Library.java
+++ src/Library.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+    private String name;
+    private List<Book> books;
+    private List<Member> members;
+
+    public Library(String name) {
+        this.name = name;
+        this.books = new ArrayList<>();
+        this.members = new ArrayList<>();
+    }
+
 
 }
