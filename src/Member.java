b7d410a80e6ef2965b3937afe221a596ca4f41db
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-25 13:55:43 +0330

    fields and constructor were written

--- src/Member.java
+++ src/Member.java
@@ -1,3 +1,21 @@
-public class Member {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Member {
+        private String memberId;
+        private String name;
+        private List<Book> borrowedBooks;
+        private Address address;
+
+    public Member(String memberId, String name, Address address) {
+        if (memberId.length() == 6) {
+            this.memberId = memberId;
+        } else {
+            System.out.println("Member ID Must be exactly 6 characters");
+            System.exit(0);
+        }
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+        this.address = address;
+    }
 }
